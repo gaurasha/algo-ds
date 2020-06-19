a7c37f1dc8dc7f32bf25aed3403b72f73eb5a4b2
Gaurav Sharma <gaurasha.gs@gmail.com>
2020-06-19 20:13:17 +0530

    adding cpp simple template

---
+++ templates/cpp/cpp-simple-template.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define LL long long int
+#define VI vector<int>
+#define PI pair<int,int>
+#define PLL pair<ll,ll> 
+#define VLL vector<long long int>
+#define MP make_pair
+#define F first
+#define S second
+#define MOD 1000000007
+#define NL ;cout<<endl;
+#define FLUSH ;cout<<flush;
+
+template<class T> ostream & operator<<(ostream &out, vector<T> &v){for(auto k : v){out<<k<<" ";}return out;}
+template<class T, class U> ostream & operator<<(ostream &out, pair<T,U> &p){out<<p.first<<" "<<p.second<<" ";return out;}
+inline vector<int> giv(int n){vector<int> v(n);for(int i=0;i<n;++i){cin>>v[i];}return v;}
+template<typename T>inline void inv(vector<T> &v,int n){for(int i=0;i<n;++i){cin>>v[i];}}
+
+template <typename T>inline void o(T t){cout << t << "\n" ;}
+template<typename T, typename... A>inline void o(T t, A... args){cout << t <<" " ;o(args...);}
+
+int main(){
+	fastio();
+	
+
+return 0;
+
+}
